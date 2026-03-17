f5f1a47bd23c2b42e66b89c76b90266dd0fb0288
Dilkush Saini <57621896+dilkushashiwal@users.noreply.github.com>
2026-03-17 17:36:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -42,11 +42,11 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-/*
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!")
 })
-*/
+
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
@@ -68,4 +68,3 @@ app.use((err, req, res, next) => {
   })
 })
 
-export default app;
